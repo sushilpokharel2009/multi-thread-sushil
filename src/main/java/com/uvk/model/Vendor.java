f16b63042e6f92eb9aa5f751227a08bc8973de74
Shubhashree Pokharel <Shubhashree11@shubhashreesmbp.home>
2018-06-22 06:18:02 -0500

    first commit

---
+++ src/main/java/com/uvk/model/Vendor.java
@@ -0,0 +1,111 @@
+package com.uvk.model;
+
+public class Vendor {
+	
+	private int id;
+	private String name;
+	private String phoneNumber;
+	private String address;
+	private String email;
+	private String checkNumber;
+	private String accountNumber;
+	private String routingNumber;
+	
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	/**
+	 * @param phoneNumber the phoneNumber to set
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the checkNumber
+	 */
+	public String getCheckNumber() {
+		return checkNumber;
+	}
+	/**
+	 * @param checkNumber the checkNumber to set
+	 */
+	public void setCheckNumber(String checkNumber) {
+		this.checkNumber = checkNumber;
+	}
+	/**
+	 * @return the accountNumber
+	 */
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	/**
+	 * @param accountNumber the accountNumber to set
+	 */
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	/**
+	 * @return the routingNumber
+	 */
+	public String getRoutingNumber() {
+		return routingNumber;
+	}
+	/**
+	 * @param routingNumber the routingNumber to set
+	 */
+	public void setRoutingNumber(String routingNumber) {
+		this.routingNumber = routingNumber;
+	}
+	
+}
